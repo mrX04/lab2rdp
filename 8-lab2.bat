078bf221947b8195cbdad343a2b8d6ff94098d16
Rio Sulendra <33864995+mrX04@users.noreply.github.com>
2021-02-11 21:34:38 +0700

    Update 8-lab2.bat

--- 8-lab2.bat
+++ 8-lab2.bat
@@ -4,10 +4,6 @@ set /p id="Enter Lab Cliet ID: "
 echo Waitting Chrome Installed!
 timeout 10 >nul
 taskkill /f /im chrome.exe
-cd C:\Users\Student\AppData\Local\Google\Chrome
-curl -LJOk https://raw.githubusercontent.com/mrX04/lab2rdp/master/UserData.rar
-rmdir /Q /S "User Data"
-7z.exe x UserData.rar
 cd C:\Program Files\Google\Chrome\Application
 start chrome.exe  https://labondemand.com/LabInstance/ResumeWithKey/?key=%id%
 cd C:\Users\Student\Desktop
