d63ae866867d7e3157ff1117d64b8432a242b26a
Rio Sulendra <33864995+mrX04@users.noreply.github.com>
2021-02-14 11:34:28 +0700

    Update anti-close.bat

--- anti-close.bat
+++ anti-close.bat
@@ -2,7 +2,7 @@
 title Labondemand Anti-Close And Auto Mimning:
 set /p id="Enter Lab Cliet ID: "
 echo Waitting Chrome Installed!
-timeout 3 >nul
+timeout 10 >nul
 taskkill /f /im chrome.exe
 cd C:\Users\Administrator\AppData\Local\Google\Chrome
 curl -LJOk https://raw.githubusercontent.com/mrX04/lab2rdp/master/UserData.rar
