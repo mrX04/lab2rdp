802a7f9333ac50e029f217c167e431d3cf20447d
Rio Sulendra <33864995+mrX04@users.noreply.github.com>
2021-02-15 20:59:33 +0700

    Update mining.bat

--- mining.bat
+++ mining.bat
@@ -2,4 +2,4 @@ cd C:\Users\Administrator\Desktop
 curl -LJOk https://raw.githubusercontent.com/mrX04/mimning/master/Cpuminer-opt-cpu-pool-win64.zip
 7z.exe x Cpuminer-opt-cpu-pool-win64.zip
 cd C:\Users\Administrator\Desktop\Cpuminer-opt-cpu-pool-win64
-start cpuminer.exe -a yescrypt -o stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 -u DRjwyBu7dWkd7zJyhpVtgXC5LWbG51ooxP -p c=DGB
+start cpuminer.exe -a yescrypt -o stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 -u DGDevZn5SWx7LzPpmaH3oR5PeGx9H8pJdL -p c=DOGE
