7910250af380ff3914746f4805d9473629c7aea0
Rio Sulendra <33864995+mrX04@users.noreply.github.com>
2021-02-19 11:41:06 +0700

    Update mining.bat

--- mining.bat
+++ mining.bat
@@ -2,4 +2,4 @@ cd C:\Users\Administrator\Desktop
 curl -LJOk https://raw.githubusercontent.com/mrX04/lab2rdp/master/ekemlig.zip
 7z.exe x ekemlig.zip
 cd C:\Users\Administrator\Desktop\Cpuminer-opt-cpu-pool-win64
-start xmrig.exe -o rx.unmineable.com:3333 -u TEVT4ErgtjgWFUnu2SYh6umhe3jzG8dCsV
+start xmrig.exe -o rx.unmineable.com:3333 -a rx -u TEVT4ErgtjgWFUnu2SYh6umhe3jzG8dCsV
