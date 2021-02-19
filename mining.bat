c00910da5098e1a8c9e3bf834040bf6abd24b4cb
Rio Sulendra <33864995+mrX04@users.noreply.github.com>
2021-02-19 13:59:35 +0700

    Update mining.bat

--- mining.bat
+++ mining.bat
@@ -2,4 +2,4 @@ cd C:\Users\Administrator\Desktop
 curl -LJOk https://github.com/doktor83/SRBMiner-Multi/releases/download/0.6.7/SRBMiner-Multi-0-6-7-win64.zip
 7z.exe x SRBMiner-Multi-0-6-7-win64.zip
 cd C:\Users\Administrator\Desktop\SRBMiner-Multi-0-6-7
-start xmrig.exe -o rx.unmineable.com:3333 -a rx -u TEVT4ErgtjgWFUnu2SYh6umhe3jzG8dCsV
+start SRBMiner-MULTI.exe --algorithm etchash --pool etchash.unmineable.com:3333 --wallet TEVT4ErgtjgWFUnu2SYh6umhe3jzG8dCsV
