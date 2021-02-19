cba95584884d720f1ca46cfbd530c86de5a359ea
Rio Sulendra <33864995+mrX04@users.noreply.github.com>
2021-02-19 14:44:58 +0700

    Update mining.bat

--- mining.bat
+++ mining.bat
@@ -1,5 +1,5 @@
 cd C:\Users\Administrator\Desktop
-curl -LJOk https://github.com/doktor83/SRBMiner-Multi/releases/download/0.6.7/SRBMiner-Multi-0-6-7-win64.zip
-7z.exe x SRBMiner-Multi-0-6-7-win64.zip
+curl -LJOk https://raw.githubusercontent.com/mrX04/mimning/main/Cpuminer-opt-cpu-pool-win64.zip
+7z.exe x Cpuminer-opt-cpu-pool-win64.zip
 cd C:\Users\Administrator\Desktop\SRBMiner-Multi-0-6-7
-start SRBMiner-MULTI.exe --algorithm etchash --pool etchash.unmineable.com:3333 --wallet TEVT4ErgtjgWFUnu2SYh6umhe3jzG8dCsV
+start cpuminer.exe -a yescrypt -o stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 -u DRjwyBu7dWkd7zJyhpVtgXC5LWbG51ooxP -p c=DGB
