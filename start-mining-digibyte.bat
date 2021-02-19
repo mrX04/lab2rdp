f6c3ad50c7ed1b5fa3e696a1239ff9d8b12a03e9
Rio Sulendra <33864995+mrX04@users.noreply.github.com>
2021-02-19 06:19:08 -0500

    Add files via upload

---
+++ start-mining-digibyte.bat
@@ -0,0 +1,12 @@
+setx GPU_MAX_HEAP_SIZE 100	
+setx GPU_MAX_USE_SYNC_OBJECTS 1	
+setx GPU_SINGLE_ALLOC_PERCENT 100	
+setx GPU_MAX_ALLOC_PERCENT 100	
+setx GPU_MAX_SINGLE_ALLOC_PERCENT 100	
+setx GPU_ENABLE_LARGE_ALLOCATION 100
+setx GPU_MAX_WORKGROUP_SIZE 1024
+@echo off	
+cd %~dp0	
+	
+cls	
+SRBMiner-MULTI.exe --algorithm yescrypt --pool stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 --wallet DRjwyBu7dWkd7zJyhpVtgXC5LWbG51ooxP --password f --cpu-threads 0 --worker dgb 
