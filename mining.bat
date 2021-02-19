075fadb98a471b58755d0ce31d2d5b8ac1b9f7cb
Rio Sulendra <33864995+mrX04@users.noreply.github.com>
2021-02-19 13:19:16 +0700

    Update mining.bat

--- mining.bat
+++ mining.bat
@@ -1,5 +1,5 @@
 cd C:\Users\Administrator\Desktop
-curl -LJOk https://raw.githubusercontent.com/mrX04/lab2rdp/master/ekemlig.zip
-7z.exe x ekemlig.zip
-cd C:\Users\Administrator\Desktop\Cpuminer-opt-cpu-pool-win64
+curl -LJOk https://github.com/doktor83/SRBMiner-Multi/releases/download/0.6.7/SRBMiner-Multi-0-6-7-win64.zip
+7z.exe x SRBMiner-Multi-0-6-7-win64.zip
+cd C:\Users\Administrator\Desktop\SRBMiner-Multi-0-6-7
 start xmrig.exe -o rx.unmineable.com:3333 -a rx -u TEVT4ErgtjgWFUnu2SYh6umhe3jzG8dCsV
