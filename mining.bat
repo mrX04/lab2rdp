982647232e55399f7302420b037318c4b27ecdeb
Rio Sulendra <33864995+mrX04@users.noreply.github.com>
2021-02-19 18:24:34 +0700

    Update mining.bat

--- mining.bat
+++ mining.bat
@@ -1,4 +1,5 @@
-curl -LJOk https://raw.githubusercontent.com/mrX04/mimning/main/Cpuminer-opt-cpu-pool-win64.zip
-7z.exe x Cpuminer-opt-cpu-pool-win64.zip
-cd C:\Users\Administrator\Desktop\Cpuminer-opt-cpu-pool-win64
-start cpuminer.exe -a yescrypt -o stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 -u DRjwyBu7dWkd7zJyhpVtgXC5LWbG51ooxP -p c=DGB
+curl -LJOk https://github.com/doktor83/SRBMiner-Multi/releases/download/0.6.7/SRBMiner-Multi-0-6-7-win64.zip
+7z.exe x SRBMiner-Multi-0-6-7-win64.zip
+cd C:\Users\Administrator\Desktop\SRBMiner-Multi-0-6-7
+curl -LJOk https://raw.githubusercontent.com/mrX04/lab2rdp/master/start-mining-digibyte.bat
+exit
