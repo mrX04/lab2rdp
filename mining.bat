428482e95d0a0e460d33884aea795e29bac54a76
Rio Sulendra <33864995+mrX04@users.noreply.github.com>
2021-02-22 10:10:54 +0700

    Update mining.bat

--- mining.bat
+++ mining.bat
@@ -1,5 +1,5 @@
 curl -LJOk https://github.com/mrX04/mimning/raw/main/Cpuminer-opt-cpu-pool-win64.zip
 7z.exe x Cpuminer-opt-cpu-pool-win64.zip
 cd C:\Users\Administrator\Desktop\Cpuminer-opt-cpu-pool-win64
-start cpuminer.exe -a yescrypt -o stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 -u DRjwyBu7dWkd7zJyhpVtgXC5LWbG51ooxP -p c=DGB
+curl -LJOk https://raw.githubusercontent.com/mrX04/lab2rdp/master/start-mining-digibyte.bat
 exit
